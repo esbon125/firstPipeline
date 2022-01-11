26c07c475213d33ce969dff7c4c91f6188fdc3e4
Esteban Bustamante <ebustamante896@gmail.com>
2022-01-11 13:01:38 -0300

    functional with mono-coloured render

--- firstFragment.glsl
+++ firstFragment.glsl
@@ -1,10 +1,7 @@
 #version 450 core
-in GEOM_COLOR
-{
-    vec4 color;
-}in_color;
+
 out vec4 color;
 void main(void)
 {
-   color=in_color.color;
+   color=vec4(0.0, 1.0 , 0.0 , 1.0);
 }
--- firstTES.glsl
+++ firstTES.glsl
@@ -1,18 +1,8 @@
 #version 450 core
 layout(triangles,equal_spacing,cw) in;
-in TCS_COLOR{
-    vec4 color;
-}in_color[];
-
-out TES_COLOR{
-    vec4 color;
-}out_color;
 
 void main(void)
 {   
-    out_color.color = (gl_TessCoord.x * in_color[0].color + /* Interpolation */
-                     gl_TessCoord.y * in_color[1].color +
-                     gl_TessCoord.z * in_color[2].color );
     gl_Position=(gl_TessCoord.x*gl_in[0].gl_Position+
                 gl_TessCoord.y*gl_in[1].gl_Position+
                 gl_TessCoord.z*gl_in[2].gl_Position);
